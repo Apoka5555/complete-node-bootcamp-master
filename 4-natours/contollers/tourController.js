1ac5f4bb6506feb3a122382a49eeb57810f98385
Abylai <a.tussupov@sapasoft.kz>
2021-10-21 13:37:15 +0600

    filtering

--- 4-natours/contollers/tourController.js
+++ 4-natours/contollers/tourController.js
@@ -5,8 +5,28 @@ const Tour = require('./../models/tourModel');
 
 exports.getAllTours = async (req, res) => {
   try {
-    const tours = await Tour.find();
+    // BUILD QUERY
+    // 1) Filtering
+    const queryObj = { ...req.query };
+    const excludedFields = ['page', 'sort', 'limit', 'fields'];
+    excludedFields.forEach((el) => delete queryObj[el]);
 
+    // 2) Advanced filtering
+
+    const queryStr = JSON.stringify(queryObj);
+
+    const query = Tour.find(queryObj);
+
+    // EXECUTE QUERY
+    const tours = await query;
+
+    // const query = Tour.find()
+    //   .where('duration')
+    //   .equals(5)
+    //   .where('difficulty')
+    //   .equals('easy');
+
+    // SEND RESPONSE
     res.status(200).json({
       status: 'success',
       results: tours.length,
